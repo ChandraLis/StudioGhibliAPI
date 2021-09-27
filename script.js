4b4abb54afd6a326ebbba920e1496b518d08ec3b
Chandra <88462784+ChandraLis@users.noreply.github.com>
2021-09-26 22:51:21 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// mix weather api with studio ghibli or cat facts
+/* base operation:
+ random selector bringing up species, name, and movie it is from, with each refresh?
+ all the cats in ghibli universe,  their info and what movie they are from?
+*/
+
+const baseURL = 'https://ghibliapi.herokuapp.com'
+let url;
+
